88cf0eb0c5c3a001fda8b6955ad098f85209aab9
Joe King <j.king92@gmail.com>
2022-10-28 16:55:22 -0700

    test: If the rooms can be listed in an array alphabetically

--- exercise_1/spec/apartment_spec.rb
+++ exercise_1/spec/apartment_spec.rb
@@ -39,4 +39,18 @@ describe Apartment do
 
     expect(apartment.rooms).to eq([laundry, bathroom, bedroom, kitchen])
   end
+
+  it 'can list the names of the rooms alphabetically' do
+    apartment = Apartment.new
+    bathroom = Room.new("bathroom")
+    laundry = Room.new("laundry")
+    kitchen = Room.new('kitchen')
+    bedroom = Room.new('bedroom')
+    apartment.add_room(laundry)
+    apartment.add_room(bathroom)
+    apartment.add_room(bedroom)
+    apartment.add_room(kitchen)
+
+    expect(apartment.list_rooms_by_name_alphabetically).to eq(["bathroom", "bedroom", "kitchen", "laundry"])
+  end
 end

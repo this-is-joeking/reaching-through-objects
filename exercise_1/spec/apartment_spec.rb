52d09b87fa5ecd01a1da0de6a6de48c1e7a47bfc
Joe King <j.king92@gmail.com>
2022-10-28 16:44:37 -0700

    test: Add tests to rent and check if apt is rented

--- exercise_1/spec/apartment_spec.rb
+++ exercise_1/spec/apartment_spec.rb
@@ -8,4 +8,19 @@ describe Apartment do
 
     expect(apt).to be_a(Apartment)
   end
+
+  it 'confirms if it is rented' do
+    apt = Apartment.new
+
+    expect(apt.is_rented?).to eq false
+  end
+
+  it 'can be rented out' do
+    apt = Apartment.new
+
+    expect(apt.is_rented?).to eq false
+    apt.rent
+
+    expect(apt.is_rented?).to eq true
+  end
 end

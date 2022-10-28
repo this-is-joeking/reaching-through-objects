4c3578014929ca099ecae408c32c72d1b0b05d3e
Joe King <j.king92@gmail.com>
2022-10-28 16:22:18 -0700

    test: Confirm room has a name

--- exercise_1/spec/room_spec.rb
+++ exercise_1/spec/room_spec.rb
@@ -3,8 +3,14 @@ require '../lib/room'
 
 describe Room do
   it 'exists' do
-    room = Room.new
+    room = Room.new("bathroom")
 
     expect(room).to be_a(Room)
+  end
+
+  it 'has a name' do
+    bedroom = Room.new("bedroom")
+
+    expect(bedroom.name).to eq("bedroom")
   end
 end

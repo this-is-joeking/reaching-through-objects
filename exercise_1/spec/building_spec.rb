511ef8bc0eb190f1fa61b33fbf99d2c1504ba798
Joe King <j.king92@gmail.com>
2022-10-28 17:08:56 -0700

    test: that apartments can be added to the building

--- exercise_1/spec/building_spec.rb
+++ exercise_1/spec/building_spec.rb
@@ -14,6 +14,24 @@ describe Building do
     building = Building.new("623", "Lazul")
 
     expect(building.building_number).to eq("623")
+  end
 
+  it 'has a name' do
+    building = Building.new("623", "Lazul")
+
+    expect(building.building_name).to eq("Lazul")
+  end
+
+  it 'can have apartments added and list them' do
+    building = Building.new("623", "Lazul")
+    apt_1 = Apartment.new
+    apt_2 = Apartment.new
+    apt_5 = Apartment.new
+
+    bldg.add_apartment(apt_1)
+    bldg.add_apartment(apt_2)
+    bldg.add_apartment(apt_5)
+
+    expect(building.list_apartments).to eq([apt_1, apt_2, apt_5])
   end
 end

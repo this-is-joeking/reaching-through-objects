39ff7bdf699c9f0075191d3d87920d49619584fe
Joe King <j.king92@gmail.com>
2022-10-29 09:07:55 -0700

    test: That the number of available apartments can be accessed in street class

--- exercise_1/spec/street_spec.rb
+++ exercise_1/spec/street_spec.rb
@@ -30,4 +30,21 @@ describe Street do
 
     expect(bell.buildings).to eq(["Lazul Apartments", "Saratoga Ridge"])
   end
+
+  it 'can provide the number of apartments available for rent' do
+    bell = Street.new("Bell Rd")
+    saratoga = Building.new("1440", "Saratoga Ridge")
+    lazul = Building.new("1350", "Lazul Apartments")
+    apt_1 = Apartment.new
+    apt_2 = Apartment.new
+    apt_3 = Apartment.new
+    bell.add_building(saratoga)
+    bell.add_building(lazul)
+    saratoga.add_apartment(apt_1)
+    saratoga.add_apartment(apt_2)
+    lazul.add_apartment(apt_3)
+
+
+    expect(bell.number_of_available_apartments).to eq(3)
+  end
 end

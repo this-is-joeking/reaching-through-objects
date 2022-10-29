de9c14ade295f2c011d696b62eacf17951005947
Joe King <j.king92@gmail.com>
2022-10-28 17:04:27 -0700

    test: That the building's number can be accessed

--- exercise_1/spec/building_spec.rb
+++ exercise_1/spec/building_spec.rb
@@ -5,8 +5,15 @@ require '../lib/building'
 
 describe Building do
   it 'exists' do
-    building = Building.new
+    building = Building.new("623", "Lazul")
 
     expect(building).to be_a Building
+  end
+
+  it 'has a number' do
+    building = Building.new("623", "Lazul")
+
+    expect(building.number).to eq("623")
+
   end
 end

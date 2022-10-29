353a3fac1cf6e33fe2617f34b9ad6950ab913469
Joe King <j.king92@gmail.com>
2022-10-29 09:53:23 -0700

    test: That street can list the buildings with avail apts and list the rooms in those apts in a hash

--- exercise_1/spec/street_spec.rb
+++ exercise_1/spec/street_spec.rb
@@ -52,4 +52,41 @@ describe Street do
     apt_1.rent
     expect(bell.number_of_available_apartments).to eq(3)
   end
+
+  it 'can list the buildings, apts, and rooms avail on the street' do
+    bell = Street.new("Bell Rd")
+    saratoga = Building.new("1440", "Saratoga Ridge")
+    lazul = Building.new("1350", "Lazul Apartments")
+    apt_1 = Apartment.new
+    apt_2 = Apartment.new
+    apt_3 = Apartment.new
+    apt_4 = Apartment.new
+    bathroom = Room.new("bathroom")
+    laundry = Room.new("laundry")
+    kitchen = Room.new('kitchen')
+    bedroom = Room.new('bedroom')
+    apt_1.add_room(bedroom)
+    apt_1.add_room(laundry)
+
+    apt_2.add_room(bedroom)
+    apt_2.add_room(bathroom)
+    apt_2.add_room(kitchen)
+    apt_2.add_room(laundry)
+
+    apt_3.add_room(bedroom)
+    apt_3.add_room(bathroom)
+    apt_3.add_room(laundry)
+
+    bell.add_building(saratoga)
+    bell.add_building(lazul)
+    saratoga.add_apartment(apt_1)
+    saratoga.add_apartment(apt_2)
+    lazul.add_apartment(apt_3)
+
+    expect(bell.list_available_apartments).to eq({
+      "Saratoga Ridge" => ["bedroom", "laundry"],
+      "Saratoga Ridge" => ["bathroom", "bedroom", "kitchen", "laundry"],
+      "Lazul Apartments" => ["bathroom", "bedroom", "laundry"]
+      })
+  end
 end

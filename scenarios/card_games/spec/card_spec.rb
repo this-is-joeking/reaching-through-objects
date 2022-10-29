adde126509540a72f3068125b0938da7fc924ee5
Joe King <j.king92@gmail.com>
2022-10-29 10:46:49 -0700

    feat: complete card class

--- scenarios/card_games/spec/card_spec.rb
+++ scenarios/card_games/spec/card_spec.rb
@@ -3,8 +3,23 @@ require '../lib/card'
 
 describe Card do
   it 'exists' do
-    card = Card.new
+    card = Card.new("spades", "2")
 
     expect(card).to be_a Card
+  end
+
+  it 'has a suit' do
+    card = Card.new("diamonds", "A")
+
+    expect(card.suit).to eq("diamonds")
+    card_2 = Card.new("spades", "2")
+
+    expect(card_2.suit).to eq("spades")
+  end
+
+  it 'has a value' do
+    card = Card.new("diamonds", "A")
+
+    expect(card.value).to eq("A")
   end
 end
